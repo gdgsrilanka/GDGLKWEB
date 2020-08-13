dc886e4eb0b5a95877839830a6807793e26f52e8
pamuditha <pamuditha.imalka@gmail.com>
2020-08-13 17:43:41 +0530

    Add files via upload

---
+++ src/views/Team.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-content class="pa-0">
+
+    <v-container fluid class="mt-3 mb-0">
+      <v-layout wrap align-center justify-center row fill-height class="my-0">
+        <v-flex xs12 md10 class="my-0">
+            <p class="google-font mb-0" style="font-size:200%;color:#616161">Our community organizers</p>
+            <p class="google-font mt-0 mb-0" style="font-size:110%">Every GDG needs to have a passionate set of people who are dedicated to providing up to date and high quality content to the community. </p>
+
+            <p class="google-font mt-3 mb-0" style="font-size:110%">Our goal is to organize spaces to connect the best of the industry with the Sri Lankan audience to boost development of local IT capabilities. Meet the people who make that happen.</p>
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid >
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10 class="">
+          <coreTeam/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid >
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10 class="">
+          <organizingTeam/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+   
+
+
+  </v-content>
+  
+</template>
+
+<script>
+import TeamDetails from '@/assets/data/chapterDetails.json'
+import coreTeam from '@/components/team/coreTeam'
+import organizingTeam from '@/components/team/organizingTeam'
+  export default {
+    components: {
+      coreTeam,
+      organizingTeam
+    },
+    data() {
+      return {
+        TeamDetails: TeamDetails,
+      }
+    },
+  }
+</script>
